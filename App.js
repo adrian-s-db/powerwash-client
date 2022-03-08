586ee7a804e2d92c4ad6ea12a7ea28d75041f6a0
Adrian Suarez del Busto <adrian.s.delbusto@gmail.com>
2022-03-08 12:45:13 +0100

    feat: pass uid to context

--- App.js
+++ App.js
@@ -1,48 +1,73 @@
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-import { auth } from "./firebaseAuth";
+import { auth } from './firebaseAuth';
+import AuthContext from './AuthContext';
 
 import HomePage from './pages/Home';
-import ScanPage from './pages/Scan'
+import ScanPage from './pages/Scan';
 import InfoPage from './pages/Info';
 import LoginPage from './pages/Login';
-import RegisterPage from './pages/Register'
+import RegisterPage from './pages/Register';
 
-import AuthorizedApp from './pages/AuthorizedApp'
-
+import AuthorizedApp from './pages/AuthorizedApp';
 
 export default function App() {
   const [user, setUser] = useState(null);
   const Stack = createStackNavigator();
 
-  useEffect(()=>{
-    const unsubscribe = auth.onAuthStateChanged(loggedInUser => {
-      setUser(loggedInUser);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((loggedInUser) => {
+      setUser(loggedInUser.uid);
     });
     return unsubscribe;
-  },[]);
+  }, []);
 
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Stack.Navigator>
-          {user ?
-          <>
-            <Stack.Screen name='Authorized' component={AuthorizedApp} options={{headerShown:false}}/>
-          </>
-          :
-          <>
-            <Stack.Screen name='Home' component={HomePage} options={{headerShown:false}}/>
-            <Stack.Screen name='Scan' component={ScanPage} options={{headerShown:false}}/>
-            <Stack.Screen name='Info' component={InfoPage} options={{headerShown:false}}/>
-            <Stack.Screen name='Login' component={LoginPage} options={{headerShown:false}}/>
-            <Stack.Screen name='Register' component={RegisterPage} options={{headerShown:false}}/>
-          </>}
-        </Stack.Navigator>
+        <AuthContext.Provider value={user}>
+          <Stack.Navigator>
+            {user ? (
+              <Stack.Screen
+                name="Authorized"
+                component={AuthorizedApp}
+                options={{ headerShown: false }}
+              />
+            ) : (
+              <>
+                <Stack.Screen
+                  name="Home"
+                  component={HomePage}
+                  options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                  name="Scan"
+                  component={ScanPage}
+                  options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                  name="Info"
+                  component={InfoPage}
+                  options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                  name="Login"
+                  component={LoginPage}
+                  options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                  name="Register"
+                  component={RegisterPage}
+                  options={{ headerShown: false }}
+                />
+              </>
+            )}
+          </Stack.Navigator>
+        </AuthContext.Provider>
       </NavigationContainer>
     </SafeAreaProvider>
-  )
+  );
 }
--- pages/AuthorizedApp.js
+++ pages/AuthorizedApp.js
@@ -1,9 +1,12 @@
+import { useContext } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 import { auth } from '../firebaseAuth';
+import AuthContext from "../AuthContext";
 
 export default function AuthorizedApp() {
+  const user = useContext(AuthContext);
   const handleSignOut = () => {
     auth
     .signOut()
@@ -15,6 +18,7 @@ export default function AuthorizedApp() {
     <View style={styles.container}>
       <Text>You are authorized!</Text>
       <Text>Your email is {auth.currentUser?.email}</Text>
+      <Text>The value of the context passed is {user}</Text>
       <Button onPress={handleSignOut} title="Log out"></Button>
       <StatusBar style="auto" />
     </View>
